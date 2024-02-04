3c7174736e34ff749199bc34c5815357eb04ea16
midlefun <vladik1997s60@gmail.com>
2024-02-05 01:56:02 +0300

    Отделение фикстур от текстов

--- conftest.py
+++ conftest.py
@@ -1,53 +1,37 @@
-# pytest_plugins = [
-#     'fixtures.page'
-# ]  #потребуется для управления фикстурами pytest.
-import pytest
-import config
-from playwright.sync_api import Browser, BrowserContext, Page, sync_playwright
+import pytest, os
+from playwright.sync_api import Playwright, Browser, BrowserContext, Page, sync_playwright
+
+class Playwright:
+    PAGE_VIEWPORT_SIZE = {'width': 1920, 'height': 1080}
+    ENV = os.getenv('ENV') if os.getenv('ENV') is not None else 'stage'
+    BROWSER = os.getenv('BROWSER') if os.getenv(
+        'BROWSER') is not None else 'chrome'  # Она отвечает за то, в каком браузере будут выполняться автотесты. По умолчанию там указан Google Chrome.
+    IS_HEADLESS = os.getenv('HEADLESS') if os.getenv('HEADLESS') is not None else False
+    SLOW_MO = int(os.getenv('SLOW_MO')) if os.getenv('SLOW_MO') is not None else 50
+    LOCALE = 'en-US'
+    DEFAULT_TIMEOUT = '2'
 
 
-@pytest.fixture()
-def page_fix() -> Page:
+@pytest.fixture(scope="class")
+def browser_fixture() -> Page:
     playwright = sync_playwright().start()
-    if config.playwright.BROWSER == 'firefox':
-        browser = get_firefox_browser(playwright)
-        context = get_context(browser)
-        page_data = context.new_page()
-    elif config.playwright.BROWSER == 'chrome':
-        browser = get_chrome_browser(playwright)
-        context = get_context(browser)
-        page_data = context.new_page()
-    else:
-        browser = get_chrome_browser(playwright)
-        context = get_context(browser)
-        page_data = context.new_page()
-    yield page_data
-    for context in browser.contexts:
-        context.close()
+    browser = playwright.chromium.launch(headless=False)
+    context = get_context(browser)
+    page = context.new_page()
+    # page.goto(link)
+    yield page
+    page.close()
     browser.close()
     playwright.stop()
 
 
-def get_firefox_browser(playwright) -> Browser:
-    return playwright.firefox.launch(
-        headless=config.playwright.IS_HEADLESS,
-        slow_mo=config.playwright.SLOW_MO,
-    )
-
-
-def get_chrome_browser(playwright) -> Browser:
-    return playwright.chromium.launch(
-        headless=config.playwright.IS_HEADLESS,
-        slow_mo=config.playwright.SLOW_MO
-    )
-
-
 def get_context(browser) -> BrowserContext:
     context = browser.new_context(
-        viewport=config.playwright.PAGE_VIEWPORT_SIZE,
-        locale=config.playwright.LOCALE
+        viewport=Playwright.PAGE_VIEWPORT_SIZE,
+        locale=Playwright.LOCALE
     )
     context.set_default_timeout(
-        timeout=config.expectations.DEFAULT_TIMEOUT
+        timeout=Playwright.DEFAULT_TIMEOUT
     )
     return context
+
