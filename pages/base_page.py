3866b46c6ba44cbfc7f582abcca057145a14b380
midlefun <vladik1997s60@gmail.com>
2024-02-09 17:36:36 +0300

    Создан метод FIND element Произведен рефакторинг кода

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,21 @@
+from playwright.sync_api import expect
+import config
+
 class BasePage:
     def __init__(self, browser_fixture, url):
         self.browser_fixture = browser_fixture
         self.url = url
 
     def open(self):
-        self.browser_fixture.goto(self.url)
+        return self.browser_fixture.goto(self.url)
 
-    # TODO Создать методы find element и find elements
+    def find_element(self, locator, timeout):
+        expect(self.browser_fixture.locator(locator),
+               f'На странице отсутствует элемент с локатором {locator}').to_be_visible(
+            timeout=timeout)
+        return self.browser_fixture.locator(locator)
 
+        # TODO Создать методы find element и find elements
 
     def pause(self):
         self.browser_fixture.pause()
